85de99b823fdc57dfc095f85021d669bdd0d387b
zhaotengfei <zhaotengfei9@gmail.com>
2017-10-10 16:19:48 +0800

    add qr

--- py/wxManager.py
+++ py/wxManager.py
@@ -22,5 +22,5 @@ def lc():
 def ec():
         print('exit')
 # itchat.auto_login()
-itchat.auto_login(hotReload=True,loginCallback=lc, exitCallback=ec)
+itchat.auto_login(enableCmdQR=True,hotReload=True,loginCallback=lc, exitCallback=ec)
 itchat.run()
