e59f57536dda3bfb51ab5657d710b80790ebda17
zhaotengfei <zhaotengfei9@gmail.com>
2017-10-10 18:54:16 +0800

    no message

--- py/wx.js
+++ py/wx.js
@@ -1,18 +1,18 @@
 
 const execFile = require('child_process').execFile;
-
-module.exports = {
-	initOrSend: funtion (data) {
-		const pyFile = '/app/test12/py/wxManager.py';
-		var params = [pyFile];
-		if (data) {
-			params.push(data);
-		}
-		const child = execFile('python', params, funtion (error, stdout, stderr) {
-			if (error) {
-				throw error;
-			}
-			console.log('1111', stdout);
-		});
+function initOrSend(data) {
+	const pyFile = '/app/test12/py/wxManager.py';
+	var params = [pyFile];
+	if (data) {
+		params.push(data);
 	}
+	const child = execFile('python', params, function (error, stdout, stderr) {
+		if (error) {
+			throw error;
+		}
+		console.log('1111', stdout);
+	});
+}
+module.exports = {
+	initOrSend
 };
